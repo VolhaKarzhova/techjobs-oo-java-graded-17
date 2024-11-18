e3d3b1f73149cd0718f6df1054653edec140e6d9
olgakorzhova <olga1584624@gmail.com>
2024-11-17 20:07:10 -0600

    add task 4

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,39 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class JobTest {
     //TODO: Create your unit tests here
+    @Test
+    public void testSettingJobId() {
+        Job firstJob = new Job();
+        Job secondJob = new Job();
+        Assert.assertNotEquals(firstJob, secondJob);
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job testJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+                new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Assert.assertTrue(testJob.getName() instanceof String);
+        Assert.assertTrue(testJob.getEmployer() instanceof Employer);
+        Assert.assertTrue(testJob.getLocation() instanceof Location);
+        Assert.assertTrue(testJob.getPositionType()instanceof PositionType);
+        Assert.assertTrue(testJob.getCoreCompetency()instanceof CoreCompetency);
+        Assert.assertEquals(testJob.getName(), "Product tester");
+        Assert.assertEquals(testJob.getEmployer().getValue(), "ACME");
+        Assert.assertEquals(testJob.getLocation().getValue(), "Desert");
+        Assert.assertEquals(testJob.getPositionType().getValue(), "Quality control");
+        Assert.assertEquals(testJob.getCoreCompetency().getValue(), "Persistence");
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        Job firstJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+                new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job secondJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+                new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Assert.assertNotEquals(firstJob, secondJob);
+    }
 }
